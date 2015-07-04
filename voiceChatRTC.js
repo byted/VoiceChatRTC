752b3620e198cabcc62e673f76cb44219e4f32bb
byted <stefan.selent@mailbox.org>
2015-07-04 17:31:16 +0200

    first version of user info exchange

--- voiceChatRTC.js
+++ voiceChatRTC.js
@@ -1,31 +1,11 @@
 var VoiceChat = (function() {
     var exports = {}
-        , videos = []
+        , participants = []
         , myStream
-        , PeerConnection = window.PeerConnection || window.webkitPeerConnection00 || window.webkitRTCPeerConnection;
+        , PeerConnection = window.PeerConnection || window.webkitPeerConnection00 || window.webkitRTCPeerConnection
+        , username, isMicMuted, areSpeakersMuted, mySocketId, htmlContainer;
 
-    function removeVideo(socketId) {
-        var video = document.getElementById('remote' + socketId);
-        if(video) {
-            videos.splice(videos.indexOf(video), 1);
-            video.parentNode.removeChild(video);
-        }
-    }
-
-    function toggleMuteOthers() {
-        videos.forEach(function(video) {
-            video.muted = !video.muted;
-        });
-        document.getElementById('voiceChat-MuteOthersBtn').textContent = videos[0].muted ? 'Unmute' : 'Mute';
-    }
-
-    function toggleMuteMe() {
-        var tracks = myStream.getAudioTracks();
-        tracks.forEach(function(track) {
-            track.enabled = !track.enabled;
-        });
-        document.getElementById('voiceChat-MuteMeBtn').textContent = tracks[0].enabled ? 'Disable Mic' : 'Enable Mic';
-    }
+    // HTML related private methods
 
     function buildHtml() {
         var body = document.getElementsByTagName('body')[0]
@@ -37,9 +17,11 @@ var VoiceChat = (function() {
             , muteOthersBtn = document.createElement('button')
             , muteMeBtn = document.createElement('button')
             , videoContainer = document.createElement('div')
+            , userContainer = document.createElement('ul')
 
         container.id = 'voiceChat';
         videoContainer.id = 'voiceChat-videos';
+        userContainer.id = 'voiceChat-user'
         roomName.id = 'voiceChat-roomName';
         muteOthersBtn.id = 'voiceChat-MuteOthersBtn';
         muteMeBtn.id = 'voiceChat-MuteMeBtn';
@@ -48,6 +30,7 @@ var VoiceChat = (function() {
         roomName.innerText = '';
         infoBar.appendChild(roomLabel);
         infoBar.appendChild(roomName);
+        infoBar.appendChild(userContainer);
 
         muteOthersBtn.textContent = 'Mute';
         muteMeBtn.textContent = 'Disable Mic';
@@ -67,11 +50,114 @@ var VoiceChat = (function() {
             roomName: roomName,
             videoContainer: videoContainer,
             muteOthersBtn: muteOthersBtn,
-            muteMeBtn: muteMeBtn
+            muteMeBtn: muteMeBtn,
+            userContainer: userContainer
         }
     }
 
-    exports.init = function(signalingServer, roomId, keepAlive) {
+    function createUserInfo(socketId) {
+        var userInfo = document.createElement('li')
+        , userName = document.createElement('span')
+        , speakersMuted = document.createElement('span')
+        , micMuted = document.createElement('span');
+        userInfo.id = 'userInfo' + socketId;        
+        userInfo.appendChild(userName);
+        userInfo.appendChild(speakersMuted);
+        userInfo.appendChild(micMuted);
+
+        return userInfo;
+    }
+
+    function createVideoTag(socketId) {
+        var videoTag = document.createElement('video');
+        videoTag.id = 'remote' + socketId;
+        videoTag.setAttribute('class', 'voiceChat remoteStream');
+        videoTag.setAttribute('autoplay', '');
+        return videoTag;
+    }
+
+// User operations: adding, removing, etc.
+
+    function addUser(socketId) {
+        var videoTag = createVideoTag(socketId)
+        , userInfoTag = createUserInfo(socketId);
+
+        htmlContainer.videoContainer.appendChild(videoTag);
+        htmlContainer.userContainer.appendChild(userInfoTag); 
+        participants.push({
+            socketId: socketId,
+            videoTag: videoTag,
+            userInfoTag: userInfoTag
+        });
+    }
+
+    function removeUser(socketId) {
+        var video = document.getElementById('remote' + socketId);
+        if(video) {
+            video.parentNode.removeChild(video);
+        }
+
+        var userInfo = document.getElementById('userInfo' + socketId);
+        if(userInfo) {
+            userInfo.parentNode.removeChild(userInfo);
+        }
+
+        participants.filter(function(participant) {
+            return participant.socketId !== socketId;
+        });
+    }
+
+    function updateUserInfo(info) {
+        var infoElement = document.getElementById('userInfo' + info.socketId);
+        if(infoElement && infoElement.childElementCount === 3) {
+            infoElement.children[0].innerText = info.username;
+            infoElement.children[1].innerText = info.isMicMuted;
+            infoElement.children[2].innerText = info.areSpeakersMuted;
+        }
+        else { console.log('WARNING: Couldn\'t update user info for', info.socketId); }
+    }
+
+// Chat operations: muting etc.
+
+    function toggleMuteOthers() {
+        participants.forEach(function(video) {
+            video.muted = !video.muted;
+        });
+        areSpeakersMuted = participants[0].muted;
+        document.getElementById('voiceChat-MuteOthersBtn').textContent = areSpeakersMuted ? 'Unmute' : 'Mute';
+    }
+
+    function toggleMuteMe() {
+        var tracks = myStream.getAudioTracks();
+        tracks.forEach(function(track) {
+            track.enabled = !track.enabled;
+        });
+        isMicMuted = !tracks[0].enabled
+        document.getElementById('voiceChat-MuteMeBtn').textContent = !isMicMuted ? 'Disable Mic' : 'Enable Mic';
+        sendMyUserInfoUpdate();
+    }
+
+// Server communication
+
+    function sendMyUserInfoUpdate() {
+        var message = {
+            eventName: 'userInfoUpdate',
+            data: {
+                username: username,
+                isMicMuted: isMicMuted || null,
+                areSpeakersMuted: areSpeakersMuted || null,
+            }
+        }
+        rtc._socket.send(JSON.stringify(message), function(error) {
+            if(error) { console.log(error); }
+            else { console.log('sent', message)};
+        });
+    }
+
+// Start the whole thing
+
+    exports.init = function(signalingServer, roomId, user, keepAlive) {
+        username = user;
         if(PeerConnection) {
             rtc.createStream({
                 "video": false,
@@ -83,42 +169,55 @@ var VoiceChat = (function() {
             alert('Your browser is not supported or you have to turn on flags. In chrome you go to chrome://flags and turn on Enable PeerConnection remember to restart chrome');
         }
 
-        var vcElements = buildHtml();
+        // atm we assume we're already connected
+        // TODO: do it after successfull connection
+        htmlContainer = buildHtml();
+        htmlContainer.roomName.innerText = roomId;
+        htmlContainer.container.style.visibility = 'visible';
+        htmlContainer.muteOthersBtn.addEventListener('click', toggleMuteOthers);
+        htmlContainer.muteMeBtn.addEventListener('click', toggleMuteMe);
 
-        vcElements.roomName.innerText = roomId;
         rtc.connect(signalingServer, roomId);
+
+        rtc.on('get_peers', function(data) {
+            console.log('[get_peers]: we\'re new and may have to connect to established users');
+            data.connections.forEach(function(socketId) { addUser(socketId); });
+            sendMyUserInfoUpdate();
+        });
+
+        rtc.on('userInfoUpdate', function(data) {
+            console.log('[userInfoUpdate]: received', data);
+            updateUserInfo(data);
+        });
+
+        rtc.on('new_peer_connected', function(data) {
+            console.log('[new_peer_connected]: new peer so create DOM for him');
+            addUser(data.socketId);
+        });
+
+        rtc.on('add remote stream', function(stream, socketId) {
+            console.log('[add remote stream]: adding remote stream for', socketId);
+            var videoTag = participants.filter(function(p) { return p.socketId === socketId; })[0].videoTag;                     
+            rtc.attachStream(stream, videoTag.id);
+        });
+
+        rtc.on('disconnect stream', function(data) {
+            console.log('remove ' + data);
+            removeUser(data);
+        });
 
         if(keepAlive) {
             setInterval(function() {
                 if (rtc._socket.bufferedAmount === 0 && rtc._socket.readyState === 1) {
                     console.log("keeping ws connection alive");
                     rtc._socket.send(JSON.stringify({
-                      "eventName": "keep-alive"
-                    }));
+                        'eventName': 'keep-alive'
+                    }), function(error){
+                      if(error){console.log(error);}
+                    });
                 }
             }, 50000);
         }
-
-        rtc.on('add remote stream', function(stream, socketId) {
-            console.log("ADDING REMOTE STREAM...");
-            var videoTag = document.createElement('video');
-            videoTag.id = 'remote' + socketId;
-            videoTag.setAttribute('class', 'voiceChat remoteStream');
-            videoTag.setAttribute('autoplay', '');
-            vcElements.videoContainer.appendChild(videoTag);
-            
-            rtc.attachStream(stream, videoTag.id);
-            videos.push(videoTag);
-        });
-
-        rtc.on('disconnect stream', function(data) {
-            console.log('remove ' + data);
-            removeVideo(data);
-        });
-
-        vcElements.container.style.visibility = 'visible';
-        vcElements.muteOthersBtn.addEventListener('click', toggleMuteOthers);
-        vcElements.muteMeBtn.addEventListener('click', toggleMuteMe);
     }
 
     return exports;
--- wsServer.js
+++ wsServer.js
@@ -1,5 +1,58 @@
 var server = require('http').createServer()
 	, webRTC = require('webrtc.io').listen(server);
 
+webRTC.rtc.on('userInfoUpdate', function(info, socket) {
+	console.log('[userInfoUpdate]:', info)
+	socket.username = info.username;
+	socket.isMicMuted = info.micMuted;
+	socket.areSpeakersMuted = info.areSpeakersMuted;
+	console.log('[userInfoUpdate]:', socket.id, 'was updated.');
+
+	info.socketId = socket.id;
+	// find the room the user belongs to
+	var listOfSocketsInRoom;
+	for(var room in webRTC.rtc.rooms) {
+		if (webRTC.rtc.rooms.hasOwnProperty(room)
+			&& webRTC.rtc.rooms[room].indexOf(socket.id) >= 0) {
+
+			listOfSocketsInRoom = webRTC.rtc.rooms[room];
+			break;
+		}
+	}
+
+	listOfSocketsInRoom.forEach(function(socketId) {
+		if(socketId !== socket.id) {
+			webRTC.rtc.getSocket(socketId).send(JSON.stringify({
+				eventName: 'userInfoUpdate',
+				data: info
+			}), function(error) {
+	            if (error) {console.log(error);}
+	            else { console.log('[userInfoUpdate]: sent update to', socketId); }
+          	});
+		}
+	});
+});
+
+
+// webRTC.on('connection', function(ws) {
+// 	ws.on('message', function(json) {
+// 		var data = JSON.parse(json);
+// 		if(data.type === 'game') {
+// 			//we expect properties 'company', 'role', 'action' on 'payload'
+// 			console.log('got ingame message from', ws.id, ': ');
+// 			console.log(data.payload);
+
+// 			webRTC.rtc.sockets.forEach(function each(client) {
+// 				console.log('send out to', client.id, ':', JSON.stringify(data.payload));
+// 				client.send(JSON.stringify({
+// 					eventName: 'game update',
+// 					data: data.payload
+// 				}));
+// 				//eventName hinzufügen
+// 			});
+// 		}		
+// 	});
+//});
+
 var port = process.env.PORT || 8090;
 server.listen(port);
